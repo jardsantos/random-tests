f7105f46bfec5a1becbed5dddda848f7485e2e57
Jardel Santos <jardelsantos44@gmail.com>
2019-06-11 23:31:08 -0300

    Add files via upload

--- ensemble.py
+++ ensemble.py
@@ -1,7 +1,7 @@
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-def ensemble_features(X_train, y_train, X_test, ratio, random_state=None, *models):
+def ensemble_features(X_train, y_train, X_test, ratio, proba=0, random_state=None, *models):
 
     '''
     Function that create new features based on an ensemble of models
@@ -21,18 +21,26 @@ def ensemble_features(X_train, y_train, X_test, ratio, random_state=None, *model
     '''
 
     n_models = len(models)
-    X_train_, X_test_, y_train_, y_train_ensemble = train_test_split(X_train,
-                                                                     y_train,
-                                                                     test_size=ratio,
-                                                                     stratify=y_train,
-                                                                     random_state=random_state)
+    if ratio == 0:
+        X_train_, X_test_, y_train_, y_train_ensemble = X_train, X_train, y_train, y_train
+    else:
+        X_train_, X_test_, y_train_, y_train_ensemble = train_test_split(X_train,
+                                                                        y_train,
+                                                                        test_size=ratio,
+                                                                        stratify=y_train,
+                                                                        random_state=random_state)
+
     X_train_ensemble = np.zeros((X_test_.shape[0],n_models))
     X_test_ensemble = np.zeros((X_test.shape[0], n_models))
 
     for i, model in enumerate(models):
 
-        model.fit(X_train_, y_train_)      
-        X_train_ensemble[:,i] = model.predict(X_test_)
-        X_test_ensemble[:,i] = model.predict(X_test)
+        model.fit(X_train_, y_train_)
+        if proba == 0:   
+            X_train_ensemble[:,i] = model.predict(X_test_)
+            X_test_ensemble[:,i] = model.predict(X_test)
+        elif proba == 1:
+            X_train_ensemble[:,i] = model.predict_proba(X_test_)[:,1]
+            X_test_ensemble[:,i] = model.predict_proba(X_test)[:,1]      
 
     return X_train_ensemble, y_train_ensemble, X_test_ensemble
